f08976de99b5f394c6d67f620e63a3b2ee254021
EnyelSequeira <enyelsequeira@hotmail.com>
2023-05-09 21:41:21 +0100

    test next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,13 +14,14 @@ export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient({}));
 
   return (
-    <SessionProvider session={pageProps.session}>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-        <Hydrate state={pageProps.dehydratedState}>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+
+      <Hydrate state={pageProps.dehydratedState}>
+        <SessionProvider session={pageProps.session}>
           <Component {...pageProps} />
-        </Hydrate>
-      </QueryClientProvider>
-    </SessionProvider>
+        </SessionProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,21 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import HomeTable from "@/components/test";
 import { Suspense } from "react";
+import { signIn, useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  console.log({ session });
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       HELLO WORLD
+      <button onClick={() => signIn("github")}>SIGN IN</button>
       <Suspense fallback={<p>LOADING.........</p>}>
         <HomeTable />
       </Suspense>
